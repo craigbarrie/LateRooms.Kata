cf3873b9414e6d8041ba5e7154c02bec1056926c
Craig Barrie <craig.barrie@data-exchange.co.uk>
2019-02-03 11:23:02 +0000

    added stock cataloge class to hold the product data

---
+++ LateRooms.checkout/Data/StockCatalogue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LateRooms.checkout.Data
+{
+    //setup some data for a stock catalogue.
+    //normally, this would be coming from a database
+    //or a service
+    public class StockCatalogue
+    {
+
+        public List<Models.SKU> Items { get; }
+
+
+        public StockCatalogue()
+        {
+        }
+    }
+}
