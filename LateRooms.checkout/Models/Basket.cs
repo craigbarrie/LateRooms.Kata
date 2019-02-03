873e7544f5bff79ec1b62f6087ee254ca6b27092
Craig Barrie <craig.barrie@data-exchange.co.uk>
2019-02-03 11:36:59 +0000

    method stubs for basket

--- LateRooms.checkout/Models/Basket.cs
+++ LateRooms.checkout/Models/Basket.cs
@@ -6,5 +6,27 @@ namespace LateRooms.checkout.Models
     public class Basket
     {
         public List<BasketItem> Items{get;set;}
+
+        Basket() {
+            InitBasket();
+        }
+
+        void InitBasket() {
+            Items = new List<BasketItem>();
+        }
+
+        public void Scan(string item) { 
+        
+        }
+
+        public int GetTotalPrice() {
+
+            //TODO:
+            throw new NotImplementedException();
+        
+        }
+
+
+
     }
 }
