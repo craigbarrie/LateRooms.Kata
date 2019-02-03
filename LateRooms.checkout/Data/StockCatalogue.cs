9255d97b754dcacd6d5990c847e8983caad6a66f
Craig Barrie <craig.barrie@data-exchange.co.uk>
2019-02-03 11:48:10 +0000

    changed stock catalogue to a static class renamed tests

--- LateRooms.checkout/Data/StockCatalogue.cs
+++ LateRooms.checkout/Data/StockCatalogue.cs
@@ -6,28 +6,17 @@ namespace LateRooms.checkout.Data
     //setup some data for a stock catalogue.
     //normally, this would be coming from a database
     //or a service
-    public class StockCatalogue
+    public static class StockCatalogue
     {
-
-        public List<Models.SKU> Items { get; private set; }
-
-
-        public StockCatalogue()
-        {
-            generateExampleData();
-        }
-
-
-        void generateExampleData() {
-
-            Items = new List<Models.SKU> {
+        public static List<Models.SKU> Items { get { 
+        
+            return new List<Models.SKU> {
 
              new Models.SKU{ SKUCode="A", UnitPrice=50, SpecialPrice=130, SpecialPriceMultiplier=3},
              new Models.SKU{ SKUCode="B", UnitPrice=30, SpecialPrice=45, SpecialPriceMultiplier=2},
              new Models.SKU{ SKUCode="C", UnitPrice=20},
-             new Models.SKU{ SKUCode="D", UnitPrice=15},
-             };
+             new Models.SKU{ SKUCode="D", UnitPrice=15},};
+            }
         }
-
     }
 }
--- checkout.tests/BasketCase.cs
+++ checkout.tests/BasketCase.cs
@@ -3,10 +3,10 @@ using Xunit;
 
 namespace checkout.tests
 {
-    public class UnitTest1
+    public class BasketCase
     {
         [Fact]
-        public void Test1()
+        public void AddOneOfEachItem()
         {
 
         }
